4003e44dd95136181619d1bba2518edf5618f16d
TR-OS <tr-os@gmx.at>
2018-06-23 12:20:47 +0200

    Add Gabriel Governor
    Signed-off-by: TR-OS <tr-os@gmx.at>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -542,6 +542,9 @@ extern struct cpufreq_governor cpufreq_gov_bioshock;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_TRIPNDROID)
 extern struct cpufreq_governor cpufreq_gov_tripndroid;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_tripndroid)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_GABRIEL)
+extern struct cpufreq_governor cpufreq_gov_gabriel;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_gabriel)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
